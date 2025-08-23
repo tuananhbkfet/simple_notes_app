0a79408d85a2cc4017a7434867842951338e2d05
Anh <tuananhbkfet@gmail.com>
2025-08-23 14:42:43 +0700

    Cập nhật tiêu đề và nhãn cho ứng dụng từ "Tuấn Anh Notes" thành "ANT Notes"

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
             <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
-            Tuấn Anh Notes
+            ANT Notes
           </h2>
           <div className="flex-1 overflow-hidden mx-4">
             <div className="whitespace-nowrap animate-marquee text-center text-yellow-400 font-bold text-2xl">
